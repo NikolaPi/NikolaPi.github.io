b3b11ece766dd7f3bda252cd720bf16673989db3
NikolaPi <pi.tesla.nikola@gmail.com>
2023-06-17 19:16:12 -0700

    updated ico to have multiple resolutions, testing config options

--- forge.config.js
+++ forge.config.js
@@ -5,19 +5,19 @@ module.exports = {
 		{
 			name: '@electron-forge/maker-squirrel',
 			config: {
-				icon: './icons/windows.ico'
+				//icon: './icons/windows.ico'
 			},
 		},
 		{
 			name: '@electron-forge/maker-deb',
 			config: {
-				icon: './icons/linux.png'
+				//icon: './icons/linux.png'
 			},
 		},
 		{
 			name: '@electron-forge/maker-rpm',
 			config: {
-				icon: './icons/linux.png'
+				//icon: './icons/linux.png'
 			},
 		},
 	],
